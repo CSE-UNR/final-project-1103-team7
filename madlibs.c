12cebb83e53553484d17cadeef6ace49c596b280
TristanP <tpulsipher@unr.edu>
2024-11-25 19:40:52 +0000

    blah

--- madlibs.c
+++ madlibs.c
@@ -1,3 +1,18 @@
 //Authors: Emma Comer, Tristan Pulsipher, Matthew Lewis
 //Final Project
 
+#include <stdio.h>
+
+#define MADLIB "madlib1.txt"
+#define MAXCOL 10
+#define MAXWIDE 100
+
+
+
+int main(){
+	char wordStore[MAXWIDE][MAXCOL];
+	
+	
+	
+	return 0;
+}
